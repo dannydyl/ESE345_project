5bc5378859f751c7e8b8a5f410c3ea8a49891839
dannydyl <dannydyl8@gmail.com>
2024-10-22 16:49:31 -0400

    added opcode package and some functionalities

--- MIPS/src/R3_instruction.vhd
+++ MIPS/src/R3_instruction.vhd
@@ -18,20 +18,91 @@
 --
 -------------------------------------------------------------------------------
 
---{{ Section below this comment is automatically maintained
---    and may be overwritten
---{entity {R3_instruction} architecture {R3_instruction}}
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.op_package.all;
 
-
-
-entity R3_instruction is
+entity R3_instruction is	 
+	port(									   
+	opcode : in std_logic_vector(7 downto 0);
+	a : in std_logic_vector(127 downto 0); -- from rs2
+	b : in std_logic_vector(127 downto 0); -- from rs1
+	result : out std_logic_vector(127 downto 0)
+	
+	);
 end R3_instruction;
 
---}} End of automatically maintained section
 
 architecture R3_instruction of R3_instruction is
+signal current_op : std_logic_vector(3 downto 0);
+signal shamt : integer := 0;
 begin
+	current_op <= opcode(3 downto 0);
+	--shamt <= to_integer()
 
-	-- Enter your statements here --
+	-- need module for CLZ, saturated arithmetic, count 1s
+
+
+
+	-- case statement for decoding opcode
+	process(opcode, a, b)
+	begin
+		case current_op is 
+			--when OP_NOP =>
+			-- no operation
+			when OP_SLHI => 
+				--result <= -- question about the shift amount
+			when OP_AU => 
+				result(127 downto 96) <= std_logic_vector(to_unsigned(a(127 downto 96)) + to_unsigned(b(127 downto 96)));
+				result(95 downto 64) <= std_logic_vector(to_unsigned(a(95 downto 64)) + to_unsigned(b(95 downto 64)));
+				result(63 downto 32) <= std_logic_vector(to_unsigned(a(63 downto 32)) + to_unsigned(b(63 downto 32)));
+				result(31 downto 0) <= std_logic_vector(to_unsigned(a(31 downto 0)) + to_unsigned(b(31 downto 0)));
+			when OP_CNTIH => 
+				result <= --something
+			when OP_AHS => 
+				result <= --something
+			when OP_AND => 
+				result <= a AND b;
+			when OP_BCW => 
+				result(127 downto 96) <= b(31 downto 0);
+				result(95 downto 64) <= b(31 downto 0);
+				result(63 downto 32) <= b(31 downto 0);
+				result(31 downto 0) <= b(31 downto 0);
+			when OP_MAXWS => 
+				result(128 downto 96) <= 
+				result(95 downto 64)
+				result(63 downto 32)
+				result(31 downto 0)
+				when OP_MINWS => 
+				result <= --something
+			when OP_MLHU => 
+				result(128 downto 96) <= std_logic_vector(to_unsigned(a(111 downto 96)) * to_unsigned(b(111 downto 96)));
+				result(95 downto 64) <= std_logic_vector(to_unsigned(a(79 downto 64)) * to_unsigned(b(79 downto 64)));
+				result(63 downto 32) <= std_logic_vector(to_unsigned(a(47 downto 32)) * to_unsigned(b(47 downto 32)));
+				result(31 downto 0) <= std_logic_vector(to_unsigned(a(15 downto 0)) * to_unsigned(b(15 downto 0)));
+			when OP_MLHCU => 
+				result(128 downto 96) <= 
+				result(95 downto 64)
+				result(63 downto 32)
+				result(31 downto 0)
+			when OP_OR => 
+				result <= a OR b;
+			when OP_CLZH => 
+				result <= --something
+			when OP_RLH => 
+				result <= --something
+			when OP_SFWU => 
+				result(127 downto 96) <= std_logic_vector(to_unsigned(a(127 downto 96)) - to_unsigned(b(127 downto 96)));
+				result(95 downto 64) <= std_logic_vector(to_unsigned(a(95 downto 64)) - to_unsigned(b(95 downto 64)));
+				result(63 downto 32) <= std_logic_vector(to_unsigned(a(63 downto 32)) - to_unsigned(b(63 downto 32)));
+				result(31 downto 0) <= std_logic_vector(to_unsigned(a(31 downto 0)) - to_unsigned(b(31 downto 0)));
+			when OP_SFHS => 
+				result <= --something
+			when others =>
+				result <= (others => '0');
+		end case;
+	end process;
+	
 
 end R3_instruction;
---
+++ MIPS/src/op_package.vhd
@@ -0,0 +1,43 @@
+-------------------------------------------------------------------------------
+--
+-- Title       : op_package
+-- Design      : MIPS
+-- Author      : Dongyun Lee
+-- Company     : Stony Brook University
+--
+-------------------------------------------------------------------------------
+--
+-- File        : //Mac/Home/Documents/SBU_2024_Fall/ESE345/ese345_project/ese345_project/MIPS/src/op_package.vhd
+-- Generated   : Tue Oct 22 15:56:14 2024
+-- From        : Interface description file
+-- By          : ItfToHdl ver. 1.0
+--
+-------------------------------------------------------------------------------
+--
+-- Description : 
+--
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+package op_package is	  
+	constant OP_NOP    : std_logic_vector(3 downto 0) := "0000";  -- NOP
+    constant OP_SLHI   : std_logic_vector(3 downto 0) := "0001";  -- SLHI: shift left halfword immediate
+    constant OP_AU     : std_logic_vector(3 downto 0) := "0010";  -- AU: add word unsigned
+    constant OP_CNTIH  : std_logic_vector(3 downto 0) := "0011";  -- CNTIH: count 1s in halfwords
+    constant OP_AHS    : std_logic_vector(3 downto 0) := "0100";  -- AHS: add halfword saturated
+    constant OP_AND    : std_logic_vector(3 downto 0) := "0101";  -- AND: bitwise logical and
+    constant OP_BCW    : std_logic_vector(3 downto 0) := "0110";  -- BCW: broadcast word
+    constant OP_MAXWS  : std_logic_vector(3 downto 0) := "0111";  -- MAXWS: max signed word
+    constant OP_MINWS  : std_logic_vector(3 downto 0) := "1000";  -- MINWS: min signed word
+    constant OP_MLUH   : std_logic_vector(3 downto 0) := "1001";  -- MLUH: multiply low unsigned
+    constant OP_MLHU   : std_logic_vector(3 downto 0) := "1010";  -- MLHU: multiply low by constant unsigned
+    constant OP_OR     : std_logic_vector(3 downto 0) := "1011";  -- OR: bitwise logical or
+    constant OP_CLZH   : std_logic_vector(3 downto 0) := "1100";  -- CLZH: count leading zeroes in halfwords
+    constant OP_RLH    : std_logic_vector(3 downto 0) := "1101";  -- RLH: rotate left bits in halfwords
+    constant OP_SFWU   : std_logic_vector(3 downto 0) := "1110";  -- SFWU: subtract from word unsigned	
+	constant OP_SFHS   : std_logic_vector(3 downto 0) := "1111";  -- SFHS: subtract from halfword saturated
+	
+end package op_package;
+
