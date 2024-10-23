906eb0ae0ab7dc0976d590ce7fefdb39bfd4ad15
dannydyl <dannydyl8@gmail.com>
2024-10-23 14:58:48 -0400

    added more modules

--- MIPS/src/CLZ.vhd
+++ MIPS/src/CLZ.vhd
@@ -25,26 +25,159 @@ use ieee.numeric_std.all;
 
 entity CLZ is		 
 	port(
-	input_hw : in std_logic_vector(15 downto 0);	 -- input should be 8 and output also should be 8
-	count		 : out std_logic_vector(15 downto 0)
+	input_hw : in std_logic_vector(127 downto 0);	 -- input should be 8 and output also should be 8
+	output_count : out std_logic_vector(127 downto 0)
 	);
 end CLZ;
 
 
 architecture CLZ of CLZ is 
+signal input_hw_index7 : std_logic_vector(15 downto 0);
+signal input_hw_index6 : std_logic_vector(15 downto 0);
+signal input_hw_index5 : std_logic_vector(15 downto 0);
+signal input_hw_index4 : std_logic_vector(15 downto 0);
+signal input_hw_index3 : std_logic_vector(15 downto 0);
+signal input_hw_index2 : std_logic_vector(15 downto 0);
+signal input_hw_index1 : std_logic_vector(15 downto 0);
+signal input_hw_index0 : std_logic_vector(15 downto 0);
+
+signal count_index7 : std_logic_vector(15 downto 0);
+signal count_index6 : std_logic_vector(15 downto 0);
+signal count_index5 : std_logic_vector(15 downto 0);
+signal count_index4 : std_logic_vector(15 downto 0);
+signal count_index3 : std_logic_vector(15 downto 0);
+signal count_index2 : std_logic_vector(15 downto 0);
+signal count_index1 : std_logic_vector(15 downto 0);
+signal count_index0 : std_logic_vector(15 downto 0);
 
 begin
-	process(input_hw)
+
+	input_hw_index7 <= input_hw(127 downto 112);
+	input_hw_index6 <= input_hw(111 downto 96);
+	input_hw_index5 <= input_hw(95 downto 80);
+	input_hw_index4 <= input_hw(79 downto 64);
+	input_hw_index3 <= input_hw(63 downto 48);
+	input_hw_index2 <= input_hw(47 downto 32);
+	input_hw_index1 <= input_hw(31 downto 16);
+	input_hw_index0 <= input_hw(15 downto 0);
+
+	output_count <= count_index7 & count_index6 & count_index5 & count_index4 & count_index3 & count_index2 & count_index1 & count_index0;
+
+	process(input_hw_index7)
 	variable count_zeros : integer := 0;
 	begin
 		for i in 15 downto 0 loop
-			if input_hw(i) = '0' then
+			if input_hw_index7(i) = '0' then
 				count_zeros := count_zeros + 1;
 			else
 				exit;
 			end if;
 		end loop;
 		
-		count <= std_logic_vector(to_unsigned(count_zeros, 16));
+		count_index <= std_logic_vector(to_unsigned(count_zeros, 16));
 	end process;
+
+	process(input_hw_index6)
+	variable count_zeros : integer := 0;
+	begin
+		for i in 15 downto 0 loop
+			if input_hw_index6(i) = '0' then
+				count_zeros := count_zeros + 1;
+			else
+				exit;
+			end if;
+		end loop;
+		
+		count_index6 <= std_logic_vector(to_unsigned(count_zeros, 16));
+	end process;
+
+	process(input_hw_index5)
+	variable count_zeros : integer := 0;
+	begin
+		for i in 15 downto 0 loop
+			if input_hw_index5(i) = '0' then
+				count_zeros := count_zeros + 1;
+			else
+				exit;
+			end if;
+		end loop;
+		
+		count_index5 <= std_logic_vector(to_unsigned(count_zeros, 16));
+
+	end process;
+	
+	process(input_hw_index4)
+	variable count_zeros : integer := 0;
+	begin
+		for i in 15 downto 0 loop
+			if input_hw_index4(i) = '0' then
+				count_zeros := count_zeros + 1;
+			else
+				exit;
+			end if;
+		end loop;
+		
+		count_index4 <= std_logic_vector(to_unsigned(count_zeros, 16));
+
+	end process;
+	
+	process(input_hw_index3)
+	variable count_zeros : integer := 0;
+	begin
+		for i in 15 downto 0 loop
+			if input_hw_index3(i) = '0' then
+				count_zeros := count_zeros + 1;
+			else
+				exit;
+			end if;
+		end loop;
+		
+		count_index3 <= std_logic_vector(to_unsigned(count_zeros, 16));
+
+	end process;
+	
+	process(input_hw_index2)
+	variable count_zeros : integer := 0;
+	begin
+		for i in 15 downto 0 loop
+			if input_hw_index2(i) = '0' then
+				count_zeros := count_zeros + 1;
+			else
+				exit;
+			end if;
+		end loop;
+		
+		count_index2 <= std_logic_vector(to_unsigned(count_zeros, 16));
+
+	end process;
+	
+	process(input_hw_index1)
+	variable count_zeros : integer := 0;
+	begin
+		for i in 15 downto 0 loop
+			if input_hw_index1(i) = '0' then
+				count_zeros := count_zeros + 1;
+			else
+				exit;
+			end if;
+		end loop;
+		
+		count_index1 <= std_logic_vector(to_unsigned(count_zeros, 16));
+
+	end process;
+	
+	process(input_hw_index0)
+	variable count_zeros : integer := 0;
+	begin
+		for i in 15 downto 0 loop
+			if input_hw_index0(i) = '0' then
+				count_zeros := count_zeros + 1;
+			else
+				exit;
+			end if;
+		end loop;
+		
+		count_index0 <= std_logic_vector(to_unsigned(count_zeros, 16));
+	end process;
+
 end CLZ;
--- MIPS/src/R3_instruction.vhd
+++ MIPS/src/R3_instruction.vhd
@@ -25,10 +25,11 @@ use work.op_package.all;
 
 entity R3_instruction is	 
 	port(									   
-	opcode : in std_logic_vector(7 downto 0);
+	opcode : in std_logic_vector(7 downto 0); -- [22:15] from R3 instruction
+	imme : in std_logic_vector(4 downto 0); -- [14:10] rs2 field in R3 instruction
 	a : in std_logic_vector(127 downto 0); -- from rs2
 	b : in std_logic_vector(127 downto 0); -- from rs1
-	result : out std_logic_vector(127 downto 0)
+	result : out std_logic_vector(127 downto 0) -- goes to rd
 	
 	);
 end R3_instruction;
@@ -37,14 +38,41 @@ end R3_instruction;
 architecture R3_instruction of R3_instruction is
 signal current_op : std_logic_vector(3 downto 0);
 signal shamt : integer := 0;
+signal CLZ_result : std_logic_vector(127 downto 0);
+signal AHS_result : std_logic_vector(127 downto 0);
+signal CNT1H_result : std_logic_vector(127 downto 0);
+signal SFHS_result : std_logic_vector(127 downto 0);
 begin
+	-- prerequisite for operations
 	current_op <= opcode(3 downto 0);
-	--shamt <= to_integer()
+	shamt <= unsigned(imme(3 downto 0));
 
 	-- need module for CLZ, saturated arithmetic, count 1s
-	CLZ_module : entity work.CLZ
+	CLZH_module : entity work.CLZ
 		port map(
+			input_hw => a,
+			output_count => CLZ_result
+		);
 
+	AHS_module: entity work.AHS
+		port map(
+			a_input => a,
+			b_input => b,
+			result_output => AHS_result
+		);
+
+	CNT1H_module: entity work.CNT1H
+		port map(
+			input_hw => a,
+			output_count => CNT1H_result
+		);
+
+	SFHS_module: entity work.SFHS
+		port map(
+			a_input => a,
+			b_input => b,
+			result_output => SFHS_result
+		);
 
 	-- case statement for decoding opcode
 	process(opcode, a, b)
@@ -53,16 +81,23 @@ begin
 			--when OP_NOP =>
 			-- no operation
 			when OP_SLHI => 
-				--result <= -- question about the shift amount
+				result(127 downto 112) <= std_logic_vector(shift_left(unsigned(a(127 downto 112)), shamt));
+				result(111 downto 96) <= std_logic_vector(shift_left(unsigned(a(111 downto 96)), shamt));
+				result(95 downto 80) <= std_logic_vector(shift_left(unsigned(a(95 downto 80)), shamt));
+				result(79 downto 64) <= std_logic_vector(shift_left(unsigned(a(79 downto 64)), shamt));
+				result(63 downto 48) <= std_logic_vector(shift_left(unsigned(a(63 downto 48)), shamt));
+				result(47 downto 32) <= std_logic_vector(shift_left(unsigned(a(47 downto 32)), shamt));
+				result(31 downto 16) <= std_logic_vector(shift_left(unsigned(a(31 downto 16)), shamt));
+				result(15 downto 0) <= std_logic_vector(shift_left(unsigned(a(15 downto 0)), shamt));
 			when OP_AU => 
-				result(127 downto 96) <= std_logic_vector(to_unsigned(a(127 downto 96)) + to_unsigned(b(127 downto 96)));
-				result(95 downto 64) <= std_logic_vector(to_unsigned(a(95 downto 64)) + to_unsigned(b(95 downto 64)));
-				result(63 downto 32) <= std_logic_vector(to_unsigned(a(63 downto 32)) + to_unsigned(b(63 downto 32)));
-				result(31 downto 0) <= std_logic_vector(to_unsigned(a(31 downto 0)) + to_unsigned(b(31 downto 0)));
+				result(127 downto 96) <= std_logic_vector(unsigned(a(127 downto 96)) + unsigned(b(127 downto 96)));
+				result(95 downto 64) <= std_logic_vector(unsigned(a(95 downto 64)) + unsigned(b(95 downto 64)));
+				result(63 downto 32) <= std_logic_vector(unsigned(a(63 downto 32)) + unsigned(b(63 downto 32)));
+				result(31 downto 0) <= std_logic_vector(unsigned(a(31 downto 0)) + unsigned(b(31 downto 0)));
 			when OP_CNTIH => 
-				result <= --something
+				result <= CNT1H_result;
 			when OP_AHS => 
-				result <= --something
+				result <= AHS_result;
 			when OP_AND => 
 				result <= a AND b;
 			when OP_BCW => 
@@ -75,13 +110,13 @@ begin
 				result(95 downto 64)
 				result(63 downto 32)
 				result(31 downto 0)
-				when OP_MINWS => 
+			when OP_MINWS => 
 				result <= --something
 			when OP_MLHU => 
-				result(128 downto 96) <= std_logic_vector(to_unsigned(a(111 downto 96)) * to_unsigned(b(111 downto 96)));
-				result(95 downto 64) <= std_logic_vector(to_unsigned(a(79 downto 64)) * to_unsigned(b(79 downto 64)));
-				result(63 downto 32) <= std_logic_vector(to_unsigned(a(47 downto 32)) * to_unsigned(b(47 downto 32)));
-				result(31 downto 0) <= std_logic_vector(to_unsigned(a(15 downto 0)) * to_unsigned(b(15 downto 0)));
+				result(128 downto 96) <= std_logic_vector(unsigned(a(111 downto 96)) * unsigned(b(111 downto 96)));
+				result(95 downto 64) <= std_logic_vector(unsigned(a(79 downto 64)) * unsigned(b(79 downto 64)));
+				result(63 downto 32) <= std_logic_vector(unsigned(a(47 downto 32)) * unsigned(b(47 downto 32)));
+				result(31 downto 0) <= std_logic_vector(unsigned(a(15 downto 0)) * unsigned(b(15 downto 0)));
 			when OP_MLHCU => 
 				result(128 downto 96) <= 
 				result(95 downto 64)
@@ -90,16 +125,16 @@ begin
 			when OP_OR => 
 				result <= a OR b;
 			when OP_CLZH => 
-				result <= --something
+				result <= CLZ_result;
 			when OP_RLH => 
 				result <= --something
 			when OP_SFWU => 
-				result(127 downto 96) <= std_logic_vector(to_unsigned(a(127 downto 96)) - to_unsigned(b(127 downto 96)));
-				result(95 downto 64) <= std_logic_vector(to_unsigned(a(95 downto 64)) - to_unsigned(b(95 downto 64)));
-				result(63 downto 32) <= std_logic_vector(to_unsigned(a(63 downto 32)) - to_unsigned(b(63 downto 32)));
-				result(31 downto 0) <= std_logic_vector(to_unsigned(a(31 downto 0)) - to_unsigned(b(31 downto 0)));
+				result(127 downto 96) <= std_logic_vector(unsigned(a(127 downto 96)) - unsigned(b(127 downto 96)));
+				result(95 downto 64) <= std_logic_vector(unsigned(a(95 downto 64)) - unsigned(b(95 downto 64)));
+				result(63 downto 32) <= std_logic_vector(unsigned(a(63 downto 32)) - unsigned(b(63 downto 32)));
+				result(31 downto 0) <= std_logic_vector(unsigned(a(31 downto 0)) - unsigned(b(31 downto 0)));
 			when OP_SFHS => 
-				result <= --something
+				result <= SFHS_result;
 			when others =>
 				result <= (others => '0');
 		end case;
